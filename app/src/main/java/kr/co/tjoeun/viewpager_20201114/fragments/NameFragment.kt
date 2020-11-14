e277dffee48a8634c4e954c8b956f0113d7a888a
say-102 <say70007@naver.com>
2020-11-14 15:23:18 +0900

    이름 / 생년 / 주소 표시용 프래그먼트들 추가

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20201114/fragments/NameFragment.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.viewpager_20201114.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.viewpager_20201114.R
+
+class NameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+}

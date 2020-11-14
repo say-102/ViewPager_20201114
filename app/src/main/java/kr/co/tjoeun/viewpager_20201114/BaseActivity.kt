802e59dc0dee0d49e9292bdba7cedc854c2198d5
say-102 <say70007@naver.com>
2020-11-14 15:06:34 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20201114/BaseActivity.kt
@@ -0,0 +1,10 @@
+package kr.co.tjoeun.lifecycle_20201114
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20201114/MainActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.viewpager_20201114
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.tjoeun.lifecycle_20201114.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
